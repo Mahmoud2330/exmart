0596a38c85c2689bb69604fdf9477a84cf811bfd
mahmoudibrahim-23 <mahmoud.ibrahim@see.com.co>
2026-09-13 05:56:57 +0300

    dividers are on the row again, so they draw as one continuous line instead of breaking at each column gap.

--- functions.php
+++ functions.php
@@ -5,7 +5,7 @@
 
 if ( ! defined( 'ABSPATH' ) ) exit;
 
-define( 'EXMART_VERSION', '1.0.65' );
+define( 'EXMART_VERSION', '1.0.66' );
 define( 'EXMART_DIR', get_template_directory() );
 define( 'EXMART_URI', get_template_directory_uri() );
 
